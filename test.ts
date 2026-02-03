adf6f3060017f853115cfb9268e114e33187b7d4
Windtalker <windtalker2@yahoo.com>
2026-02-03 12:41:00 -0600

    Update
    Added and updated files from ruthsarian's clone.

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+basic.showString("X")
+
+input.onButtonPressed(Button.A, () => {
+    bluetooth.activateSwgeLocationBeacon(1)
+    basic.showString("A")
+})
+
+input.onButtonPressed(Button.B, () => {
+    bluetooth.activateGenericBeacon("387", "HELLO");
+    basic.showString("B")
+})
